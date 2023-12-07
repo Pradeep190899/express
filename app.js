2236ec902a442653661292d48346d26198d3edd2
pradeep190899 <pradeep98singh98@gmail.com>
2023-12-07 17:15:47 +0530

    added form

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-app.use((req,res,next)=>{
-    console.log('in the middlewaare');
-    next();
-});
-app.use((req,res,next)=>{
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+app.use("/home",(req,res ,next)=>{
+    console.log("in home")
+    res.send('<h1>welcome home</h1>')
+})
+app.use('/add-product',(req,res)=>{
+
+res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+
+})
+app.use("/product",(req,res)=>{
+    console.log(req.body)
+    res.redirect("/")
+})
+app.use( "/",(req,res,next)=>{
     console.log('in other middleware');
 res.send('<h1>hello sir</h1>')
+next() ;
 });
-const server = http.createServer(app);
-server.listen(3000);
+
+app.listen(3000);
